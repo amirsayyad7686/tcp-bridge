e6ef9005844e1a25962a173831612b6783a9c202
amir sayyad <amirsayyad7686@gmail.com>
2024-12-03 07:54:23 -0800

    'amir'

--- socket.js
+++ socket.js
@@ -20,24 +20,25 @@ const tcpServer = net.createServer((socket) => {
             // Parse the incoming data (e.g., "client23832:2943.5274,05227.6750,asdasdasd")
             const message = data.toString().trim();
             const [clientId, coordinates] = message.split(":");
-            console.log(coordinates);
-            // Split the data by commas
-            const splitData = coordinates.split(',');
-            var integerValue = 0
-                // Check if index 4 exists
-            if (splitData[3]) {
-                // Convert the 4th index (0-based index 3) to an integer
-                integerValue = parseInt(splitData[3], 10);
 
-                if (!isNaN(integerValue)) {
-                    console.log("Integer value:", integerValue);
-                } else {
-                    console.error("The value at index 4 is not a valid integer.");
-                }
-            } else {
-                console.error("Index 4 does not exist in the data.");
-            }
             if (clientId === 'client23832') {
+                console.log(coordinates);
+                // Split the data by commas
+                const splitData = coordinates.split(',');
+                var integerValue = 0
+                    // Check if index 4 exists
+                if (splitData[3]) {
+                    // Convert the 4th index (0-based index 3) to an integer
+                    integerValue = parseInt(splitData[3], 10);
+
+                    if (!isNaN(integerValue)) {
+                        console.log("Integer value:", integerValue);
+                    } else {
+                        console.error("The value at index 4 is not a valid integer.");
+                    }
+                } else {
+                    console.error("Index 4 does not exist in the data.");
+                }
                 // If the clientId matches, join the corresponding room in Socket.IO
                 const roomId = '67437be2b177696c9afb3594'; // Room ID associated with this client
                 console.log(`Client joined room: ${roomId}`);
